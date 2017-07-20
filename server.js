88850d057ae3f9bf334992d6e2a2a0f258522ab9
Kimberley Johnson <kimberleyjohnson@Kimberleys-MacBook-Pro.local>
2017-07-20 12:25:39 -0500

    updated heading tag

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get("/scrape", function(req, res) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(html);
     // Now, we grab every h2 within an article tag, and do the following:
-    $(".cd__headline").each(function(i, element) {
+    $("h3.cd__headline").each(function(i, element) {
 
       // Save an empty result object
       var result = {};
